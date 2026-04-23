c1243105af2f0516ad26ed951feb8155497423a8
aka-hombre <iarodr13@asu.edu>
2026-04-23 14:47:21 -0700

    added dir for manifest, and dummy file

--- tests/testing.py
+++ tests/testing.py
@@ -1,6 +1,7 @@
 import networkx as nx
 from os import getcwd
-import matplotlib.pyplot as plt
+
+import pandas as pd 
 
 
 graphs = []
@@ -12,10 +13,14 @@ with open("graph_data/V6/graphs6.g6")as f:
         print(line.strip)
         Gx = nx.parse_graph6(line.strip())
         graphs.append(Gx)
-"""
+
 
 graphs = nx.read_graph6("graph_data/V6/graphs6.g6")
 print(len(graphs))
 plt.figure()
 nx.draw(graphs[-1])
 plt.show()
+"""
+
+df = pd.read_parquet("graphs_manifest.parquet")
+print(df.head())
