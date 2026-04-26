1ba1b356fc3b5448eae4a09f83b2cd0633439eb9
aka-hombre <iarodr13@asu.edu>
2026-04-25 19:33:58 -0700

    updated module for best practices

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -4,4 +4,6 @@ __all__ = ["build_manifest", "data_manager", "graph_grappler"]
 
 from . import build_manifest
 
-from . import data_manager
+from .data_manager import DataManager, GraphDataSet
+
+from .graph_grappler import get_graphs
