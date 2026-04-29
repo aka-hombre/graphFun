9b2661535c376da51f62acf3c4837f5d082c8a7d
aka-hombre <iarodr13@asu.edu>
2026-04-29 12:34:47 -0700

    more unit testing, still need to impliment attention

--- graphfun/models/models.py
+++ graphfun/models/models.py
@@ -1,10 +1,15 @@
 import torch
 import torch.nn as nn
 
+#-----
+#   Models
+#-----
+
 class myLinearModel(nn.Module):
     """
-    Classic Linear model: Since matrices cannot be passed through Linear, we flatten
+    Classic Linear model: with flatten, can we pass through tensor? 
         - 10x10 to 100 to 2
+        - get_graphs now collects flatten vectors, so nn.Flatten() is unecessary
     """
     def __init__(self, in_dimension=100, classes= 2):
         super().__init__()
@@ -30,6 +35,22 @@ class myMLP(nn.Module):
         )
     def forward(self, x):
         return self.model(x)
+    
+class myLinearWithAtt(nn.Module):
+    """
+    0_0     
+    """
+    def __init__(self, in_dimension=10, classes= 2):
+        super().__init__()
+        self.model = nn.Linear(in_dimension, classes)
+
+        def forward(self, x):
+            return self.model(self.to_vec(x))
+
+    def to_vec(X):
+        return torch.split(X, 10, dim=0)    #   dim=0 corresponds to splitting by rows
+
+    
 
 
 # ------------------------
@@ -38,7 +59,8 @@ class myMLP(nn.Module):
 
 MODEL_REGISTRY = {
     "linear": myLinearModel,
-    "MLP": myMLP
+    "MLP": myMLP,
+    "linear_w_att": myLinearWithAtt
 }
 
 def get_model(name: str, **kwargs) -> nn.Module:
